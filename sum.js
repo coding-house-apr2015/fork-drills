55f897aca81d5ac7e881ef10e91b248f30c4f0c0
Chris Oyler <christopher.oyler@gmail.com>
2015-04-08 10:32:57 -0700

    stuff

--- sum.js
+++ sum.js
@@ -1,4 +1,4 @@
-function sum(x, y){
+/*function sum(x, y){
   return x + y;
 }
 
@@ -6,3 +6,17 @@ function sum(x, y){
 
 var s = sum(3, 4);
 console.log('the sum of 3 and 4 is', s);
+*/
+
+
+var s = '3;4;5';
+var s = s.split(';');
+
+var o = cubeNums(s);
+
+function cubeNums (str){
+  for(var i = 0; i < str.length; i++){
+    Math.pow(parseInt(str[i], 3));
+  }
+  return(o);
+}
